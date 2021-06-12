67ae2c8ebb82e9f6b46ff0aae483cf4ee2443e0c
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-06-12 15:51:51 -0500

    function to get the distribution of positives and negatives

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -24,18 +25,45 @@ func entropy(p, n float64, t int) (s float64) {
 	return s
 }
 
+func data_Sv(field, values []string, attr_main []bool) (ps_ns [][]int) {
+	p := 0
+	n := 0
+	for _, v1 := range values {
+		for i, v2 := range field {
+			if v1 == v2 && attr_main[i] {
+				p++
+			} else if v1 == v2 && !attr_main[i] {
+				n++
+			}
+		}
+		ps_ns = append(ps_ns, []int{p, n})
+		p = 0
+		n = 0
+	}
+	return ps_ns
+
+}
+
 func main() {
 
-	sky := []string{"Soleado", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Soleado", "Soleado", "Lluvioso", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso"}
-	sky_values := []string{"Soleado", "Lluvioso"}
-	temp := []string{"Alta", "Alta", "Alta", "Media", "Fria", "Fria", "Fria", "Media", "Fria", "Media", "Media", "Media", "Alta", "Media"}
-	temp_values := []string{"Alta", "Media", "Fria"}
-	hum := []string{"Alta", "Alta", "Alta", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Alta"}
-	hum_values := []string{"Alta", "Normal"}
+	// sky := []string{"Soleado", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Soleado", "Soleado", "Lluvioso", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso"}
+	// sky_values := []string{"Soleado", "Lluvioso"}
+
+	// temp := []string{"Alta", "Alta", "Alta", "Media", "Fria", "Fria", "Fria", "Media", "Fria", "Media", "Media", "Media", "Alta", "Media"}
+	// temp_values := []string{"Alta", "Media", "Fria"}
+
+	// hum := []string{"Alta", "Alta", "Alta", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Alta"}
+	// hum_values := []string{"Alta", "Normal"}
 
 	wind := []string{"Debil", "Fuerte", "Debil", "Debil", "Debil", "Fuerte", "Fuerte", "Debil", "Debil", "Debil", "Fuerte", "Fuerte", "Debil", "Fuerte"}
 	wind_values := []string{"Debil", "Fuerte"}
 
 	play_tennis := []bool{false, false, true, true, true, false, true, false, true, true, true, true, true, false}
 
+	p, n, t := p_and_n(play_tennis)
+
+	entropy_s := entropy(p, n, t)
+
+	fmt.Println(entropy_s)
+	fmt.Println(data_Sv(wind, wind_values, play_tennis))
 }
