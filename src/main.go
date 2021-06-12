fc2643e4dd0046914cd604d83f46e90301b712e9
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2021-06-12 15:39:24 -0500

    declared fields and attributes

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+func main() {
+
+	sky := []string{"Soleado", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Lluvioso", "Soleado", "Soleado", "Lluvioso", "Soleado", "Lluvioso", "Lluvioso", "Lluvioso"}
+	sky_values := []string{"Soleado", "Lluvioso"}
+	temp := []string{"Alta", "Alta", "Alta", "Media", "Fria", "Fria", "Fria", "Media", "Fria", "Media", "Media", "Media", "Alta", "Media"}
+	temp_values := []string{"Alta", "Media", "Fria"}
+	hum := []string{"Alta", "Alta", "Alta", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Normal", "Normal", "Alta", "Normal", "Alta"}
+	hum_values := []string{"Alta", "Normal"}
+
+	wind := []string{"Debil", "Fuerte", "Debil", "Debil", "Debil", "Fuerte", "Fuerte", "Debil", "Debil", "Debil", "Fuerte", "Fuerte", "Debil", "Fuerte"}
+	wind_values := []string{"Debil", "Fuerte"}
+
+	play_tennis := []bool{false, false, true, true, true, false, true, false, true, true, true, true, true, false}
+
+}
